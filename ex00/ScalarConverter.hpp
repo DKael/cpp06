a3e5c9b2fdb1022ed1ffa69995a6a5fe02abebe1
Kael <kael@Mac-Studio.local>
2024-02-19 19:24:48 +0900

    start cpp06

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hyungdki <hyungdki@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 14:09:22 by hyungdki          #+#    #+#             */
+/*   Updated: 2024/02/19 14:10:01 by hyungdki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+class ScalarConverter {
+
+
+};
+
+#endif
